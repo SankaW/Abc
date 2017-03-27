de6c65c4b0fd0c24b5e2682fd2b483ec5a8a99ef
Sanka Weerathunga <sdweerathunga5@gmail.com>
2017-03-27 14:59:28 +0530

    CompanyRepository update

--- SW01/SW01.Data/SW01Repository/CompanyRepository.cs
+++ SW01/SW01.Data/SW01Repository/CompanyRepository.cs
@@ -1,41 +1,47 @@
-using SW01.Data.ISW01Repository;
- 
-namespace SW01.Data.SW01Repository
+﻿using System;
+using SW01.Data.ISW01Repository;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SW01.Data.SW01Repository;
+
+namespace SW01.Data.SW01Repository
 {
-    public class CompanyRepository : GenericRepository<Company>, ICompanyRepository
-    {
+    public class CompanyRepository : GenericRepository<Company>, ICompanyReository
+    {
 
-	   private bool disposed = false;
- 
-        public CompanyRepository(SW01Entities context)
-            : base(context)
-        {
- 
-        }
- 
+        private bool disposed = false;
 
- 
-        #region Dispose Function
- 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
- 
-        private void Dispose(bool disposing)
-        {
-            if (!disposed)
-            {
-                if (disposing)
-                {
-                    if (Context != null)
-                        Context.Dispose();
-                }
-                disposed = true;
-            }
-        }
- 
-        #endregion
-    }
+        public CompanyRepository(SW01Entities context)
+            : base(context)
+        {
+
+        }
+
+
+
+        #region Dispose Function
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (Context != null)
+                        Context.Dispose();
+                }
+                disposed = true;
+            }
+        }
+
+        #endregion
+    }
 }
